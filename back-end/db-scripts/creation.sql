ac7a4678f5ff12f84faa463fb068bfb27595c462
Vinícius Martins <vinicius.mp.silva@outlook.com>
2026-02-02 23:34:09 -0300

    add db scripts for creation and seed initial data

---
+++ back-end/db-scripts/creation.sql
@@ -0,0 +1,16 @@
+create table color(
+  id uuid primary key,
+  name varchar(100) not null
+);
+
+create table patient(
+  id uuid primary key,
+  first_name varchar(50) not null,
+  last_name varchar(50) not null,
+  gender varchar(10),
+  age int,
+  favorite_color_id uuid,
+  created_at timestamp not null,
+  updated_at timestamp,
+  constraint fk_patient_color foreign key(favorite_color_id) references color(id)
+);
